a1586c948cf1e034921ec220db0b230350aa73f3
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-14 15:26:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,7 +14,7 @@ def nyc_pigeon_organizer(data)
     end
   end
   #Create a variable for the attributes of the pigeosn
-  attribute_keys = id.keys
+  pigeon_list = id.keys
   #For each color, list the names of the pigeons with corresponding color
   data[:color].each do |bird_color, bc_name_list|
     #Iterate through the list of names(name) for each pigeon(bird_name)
